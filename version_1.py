63cd4cd7808513a7497a3a9a447602f5c5b3d6c6
algoccs <algodemoacc@gmail.com>
2026-09-24 14:33:31 -0400

    Refactor game sprite initialization and game loop

--- version_1.py
+++ version_1.py
@@ -1,55 +1,78 @@
 import pygame
+
 pygame.init()
 
-# Parametros
-ANCHO, ALTO = 500, 500
-COLOR_FONDO = (54, 74, 224)
+# Constantes
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+BACKGROUND = (241, 120, 203)
 
-# Pantalla (display)
-screen = pygame.display.set_mode((ANCHO, ALTO))
-reloj = pygame.time.Clock()
+vel_pelota_x = 5
+vel_pelota_y = 5
 
-# Clase principal
+
 class GameSprite():
-    def __init__(self, file_img, x, y, ancho, alto):
-        self.ancho = ancho
-        self.alto = alto
-        self.image = pygame.transform.scale(pygame.image.load(file_img), (self.ancho, self.alto))
-        self.rect = pygame.Rect(x, y, ancho, alto)
-        self.rect.x = x
-        self.rect.y = y
+    def __init__(self, sprite_image, x_pos, y_pos, sprite_width, sprite_height):
+        self.width = sprite_width
+        self.height = sprite_height
+        self.image = pygame.transform.scale(pygame.image.load(sprite_image), (self.width, self.height))
+        self.rect = pygame.Rect(x_pos, y_pos, sprite_width, sprite_height)
+        self.rect.x = x_pos
+        self.rect.y = y_pos
 
     def draw(self):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
-# Objetos (instancias)
-player = GameSprite('pacman.png', 200, 400, 50, 50)
-ball = 'no tengo'
+    # Metodo que nos permite establecer colisiones entre objetos
+    def collidepoint(self, x_pos, y_pos):
+        return self.rect.collidepoint(x_pos, y_pos)
+    
+    def colliderect(self, rect):
+        return self.rect.colliderect(rect)
 
-# ENEMIGOS
-enemy_x, enemy_y = 5, 5
+
+# Main screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Arkanoid')
+
+
+# Objetos
+player = GameSprite('player.png', 200, 400, 104, 25)
+ball = GameSprite('ball.png', 200, 370, 30, 30)
+
+# Creacion de enemigos
+enemy_x, enemy_y = 10, 5
 count = 9
-monsters = [] # Lista vacia, para llenarla de objetos (enemigos)
+monsters = []
 
 for i in range(3):
     y = (55 * i) + enemy_y
-    x = (27.5 * i) + enemy_x
-    for e in range(count):
-        enemy = GameSprite('ghost.png', x, y, 50, 50)
+    x = (27.5 *i) + enemy_x
+
+    for enemy in range(count):
+        enemy = GameSprite('enemy.png', x, y, 40, 40)
         monsters.append(enemy)
         x += 55
     count -= 1
+        
+# Game Loop
+clock = pygame.time.Clock()
+running = True
+finish = False
 
-# Ciclo de juego
-run = True
-while run:
-    screen.fill(COLOR_FONDO)
-    player.draw() # DIBUJA A PLAYER
-    for enemy in monsters:
-        enemy.draw()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False    
+    
+    if not finish:
+        screen.fill(BACKGROUND)
+        for monster in monsters:
+            monster.draw()
+            
+        player.draw()
+        ball.draw()
 
     pygame.display.update()
-    reloj.tick(60)
+    clock.tick(60)
 
 pygame.quit()
-
